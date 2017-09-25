529cf1ed50f7b53906e70302a780d3aaa5b88338
Arkatch <32023460+Arkatch@users.noreply.github.com>
2017-09-25 21:31:35 +0200

    Add files via upload

--- kod.js
+++ kod.js
@@ -1,50 +1,100 @@
-var myGamePiece, getWeapon, getRangedWeapon, weaponONE = true, weaponTWO = true, staticArrow = true, target, heroSpeed = 5, targetPoints = 0, arrowCrashTest = [], arrowCrashStatic = [], crashLength = 0, backgroundALL, jump = true, targetMove = 0, targetTopBot = true, fixedY = [];
+//INSTRUKCJA//
+/*
+	Tworzenie nowego nieruchomego obiektu:
+	-stwórz zmienną w punkcie 2. np "x"
+	-zmienna z punktu 2. "x" ma "= new component(width, height, color, x, y);" w funkcji startGame()
+	-do funkcji updateGameArea() wywołaj funkcje z wcześniejszymi zmiennymi: ArrowCrashFixed(x);
+*/
+/*
+	Tworzenie nowego ruchomego obiektu:
+	-stwórz zmienną w punkcie 2. np "x"
+	-zmienna z punktu 2. "x" ma "= new component(width, height, color, x, y);" w funkcji startGame()
+	-do funkcji updateGameArea() wywołaj funkcje z wcześniejszymi zmiennymi: ArrowCrashFixed(x);
+	-w funkcji updateGameArea() dodaj objectMove(x, 30, 110); liczby 30 i 110 to wysokość po jakiej rusza się nasz obiekt
+*/
+
+/*1.Globalne zmienne ogólne*/ 
+var staticArrow = true, targetPoints = 0, jump = true, targetMove = 0, Myscore;
 var red = document.getElementById('hero');
-var mapDOWN = {};
-var mapPRESS = {};
-var lastkey = false;
-function startGame() {
-    myGameArea.start();
-    myGamePiece = new hero(0, 0, 300, 92);
-    getWeapon = new sword(0, 50);
-	target = new component(40, 40, "white", 550, 90);
-	backgroundALL = new component(960, 475, "black", 0, 150);
-	getRangedWeapon = new bow();
-}
+
+/*2.Obiekty na mapie*/
+var MyHero, target, target2, target3, target4, target5, backgroundBottom, backgroundTop;
+
+/*3.Zmienne broni*/
+var getWeapon, getRangedWeapon, weaponONE = false, weaponTWO = false;
+ 
+/*Zmienne sterowania*/
+var mapDOWN = {}, mapPRESS = {}, lastkey = false,  heroSpeed = 5;
+
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 960;
-        this.canvas.height = 540;
-		this.canvas.style.border = "solid 2px black";
-		this.canvas.style.backgroundColor = "gray";
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea,20);
+    canvas 	: document.createElement("canvas"),
+    start 	: function() {
+				this.canvas.width = 670;
+				this.canvas.height = 540;
+				this.canvas.style.border = "solid 2px black";
+				this.canvas.style.backgroundColor = "gray";
+				this.context = this.canvas.getContext("2d");
+				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+				this.interval = setInterval(updateGameArea,20);
 		
-        window.addEventListener('keydown', function (e) {
-			e = e || event;
-			mapDOWN[e.keyCode] = e.type == 'keydown';
-        }, false)
-        window.addEventListener('keyup', function (e) {
-            e = e || event;
-			mapDOWN[e.keyCode] = e.type == false;
-        })
-		window.addEventListener('keypress', function (e) {
-            e = e || event;
+			window.addEventListener('keydown', function (e) {
+				e = e || event;
+				mapDOWN[e.keyCode] = e.type == 'keydown';
+			}, false)
+			window.addEventListener('keyup', function (e) {
+				e = e || event;
+				mapDOWN[e.keyCode] = e.type == false;
+			})
+			window.addEventListener('keypress', function (e) {
+				e = e || event;
 			mapPRESS[e.keyCode] = e.type == 'keypress';
-        }, false)
+			}, false)
     }, 
     clear : function(){
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
 	score : function(){
-		let scoreVAR = myGameArea.context;
-		scoreVAR.font = "30px Arial"
-		scoreVAR.fillStyle = "white"
-		scoreVAR.fillText(crashLength, 10, 465);
+				let scoreVAR = myGameArea.context;
+				Myscore = target5.value-(target.value+target2.value+target3.value+target4.value);
+				scoreVAR.font = "30px Arial"
+				scoreVAR.fillStyle = "white"
+				scoreVAR.fillText(Myscore, 45, 505);
 	}
 }
+
+function addObject() {
+    myGameArea.start();
+	backgroundTop = new component(690, 40, "red", 0, 0);
+	backgroundBottom = new component(690, 475, "black", 0, 170);
+	let floorY = backgroundBottom.y - 58;
+    MyHero = new hero(0, 58, 30, floorY);
+    getWeapon = new sword(0, 50);
+	getRangedWeapon = new bow();
+	target = new createTargetObject(40, 40, "white", 180, 90);
+	target2 = new createTargetObject(40, 40, "red", 280, 90);
+	target3 = new createTargetObject(40, 40, "red", 380, 90);
+	target4 = new createTargetObject(40, 40, "black", 480, 90);
+	target5 = new createTargetObject(40, 40, "black", 580, 50);
+}
+
+function createTargetObject(width, height, color, x, y){
+	this.arrowCrashFixedTest = [];
+	this.arrowCrashFixedStatic = [];
+	this.fixedY = [];
+	this.value = 0;
+	this.bool = true;
+	
+	let ctx = myGameArea.context;
+    this.width = width;
+    this.height = height;   
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    } 
+}
+
 function component(width, height, color, x, y) {
 	let ctx = myGameArea.context;
     this.width = width;
@@ -56,8 +106,10 @@ function component(width, height, color, x, y) {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }  
 }
+
 function hero(width, height, x, y) {
 	let ctx = myGameArea.context;
+	
     this.gamearea = myGameArea;
     this.width = width;
     this.height = height;
@@ -69,10 +121,15 @@ function hero(width, height, x, y) {
 	this.hY;
 	this.gravity = 0;
     this.gravitySpeed = 0;
+	
+	let floorY = backgroundBottom.y - 58;
+	let topY = backgroundTop.height+20;
+	
+	
     this.update = function() {
 		ctx.drawImage(red, this.x, this.y);
     }
-    this.newPos = function() {
+    this.jumpHero = function() {
 		if(jump){
 			this.gravitySpeed += this.gravity;
 			this.y += this.speedY+this.gravitySpeed;
@@ -80,8 +137,8 @@ function hero(width, height, x, y) {
 				jump = false;
 			}
 		}else{
-			if(this.y>=92){
-				this.y = 92;
+			if(this.y>=floorY){
+				this.y = floorY;
 				this.gravitySpeed = 0;
 				lastkey = false;
 			}
@@ -98,6 +155,7 @@ function hero(width, height, x, y) {
 		return this.hY = (this.y+32+this.speedY);
 	}
 }
+
 function sword(longX, longY){
 	this.gamearea = myGameArea;
 	let ctx = myGameArea.context;
@@ -115,8 +173,8 @@ function sword(longX, longY){
 	let rotateBOT = 0;
 	
 	this.hand = function() {
-		this.x = myGamePiece.handPosX();
-		this.y = myGamePiece.handPosY();
+		this.x = MyHero.handPosX();
+		this.y = MyHero.handPosY();
 	}
 	this.attack1 = function() {
 		gravitySpeed += gravity;
@@ -178,13 +236,20 @@ function sword(longX, longY){
 		bot2.stroke(); 
 	}
 }
+
 function bow(){
 	this.gamearea = myGameArea;
+	
+	//kij łuku
 	let ctx = myGameArea.context;
+	
+	//cięciwy
 	let ctx2 = myGameArea.context;
 	let ctx3 = myGameArea.context;
-	let arrow = myGameArea.context;
+	//
 	
+	//strzała
+	let arrow = myGameArea.context;
 	
 	this.gravity = 0.03;
     this.gravitySpeed = 0;
@@ -198,10 +263,9 @@ function bow(){
 	let temp2 = 0;
 	let arrowXTemp = 0;
 	
-	
 	this.hand = function() {
-		this.x = myGamePiece.handPosX()+2;
-		this.y = myGamePiece.handPosY()-10;
+		this.x = MyHero.handPosX()+2;
+		this.y = MyHero.handPosY()-10;
 	}
 	this.chord = function() {
 		this.gravitySpeed += this.gravity;
@@ -279,71 +343,80 @@ function bow(){
 			arrow.stroke();
 		}
 	}
-	this.crashArrow = function(target) {
-		let targetleft = target.x;
-		let targettop = target.y;
-		let targetbottom = target.y + (target.height);
-		let targetright = target.x + (target.width);
-		
-		let myleft = getRangedWeapon.xCAS+40;
-		let mytop = getRangedWeapon.yT12;
-		
-		
-		let crash = false;
-		if ((myleft > targetleft) && (myleft < targetright) && (mytop > targettop) && (mytop < targetbottom)){
-			crash = true;
-		}
-		return crash;
-	}
-	this.drawArrow = function(staticY, fixedY) {
-		let newArrow = myGameArea.context;
-		let targetleft = target.x;
-		let mytop = staticY+(target.y - fixedY);
+}
 
-		newArrow.beginPath();
-		newArrow.moveTo(targetleft, mytop);
-		newArrow.lineTo(targetleft-37, mytop);
-		newArrow.strokeStyle = "black";
-		newArrow.stroke();
+function ArrowCrashFixed(targetObject){
+	if(crashTest(targetObject) && !(staticArrow)){
+		targetObject.arrowCrashFixedTest[targetObject.value] = false;
+		targetObject.arrowCrashFixedStatic[targetObject.value] = getRangedWeapon.yT12;
+		targetObject.fixedY[targetObject.value] = targetObject.y;
+		targetObject.value++;
+		getRangedWeapon.reLoadArrow();
+	}
+	for(var i = 0, j = targetObject.arrowCrashFixedTest.length;i < j; i++)
+	drawFixedArrow(targetObject.arrowCrashFixedStatic[i], targetObject.fixedY[i], targetObject);
+	targetObject.update();
+}
+
+function drawFixedArrow(staticY, fixedY, targetObject) {
+	let newArrow = myGameArea.context;
+	let targetleft = targetObject.x;
+	let mytop = staticY+(targetObject.y - fixedY);
+	newArrow.beginPath();
+	newArrow.moveTo(targetleft, mytop);
+	newArrow.lineTo(targetleft-37, mytop);
+	newArrow.strokeStyle = "black";
+	newArrow.stroke();
+}
+
+function crashTest(objectName){
+	let targetleft = objectName.x;
+	let targettop = objectName.y;
+	let targetbottom = objectName.y + (objectName.height);
+	let targetright = objectName.x + (objectName.width);
+		
+	let myleft = getRangedWeapon.xCAS+40;
+	let mytop = getRangedWeapon.yT12;
+		
+	let crash = false;
+	if ((myleft > targetleft) && (myleft < targetright) && (mytop > targettop) && (mytop < targetbottom)){
+		crash = true;
+	}
+	return crash;
+}
+
+function objectMove(objectName, yTop, yBot, speed){
+	if(objectName.bool){
+		objectName.y+=speed;
+		if(objectName.y == yBot){
+			objectName.bool=false;
+		}
+	}
+	if(!objectName.bool){
+		objectName.y-=speed;
+		if(objectName.y == yTop){
+			objectName.bool=true;
+		}
 	}
 }
-function updateGameArea() {
-    myGameArea.clear();
-	if(targetTopBot){
-		target.y+=0.5;
-		if(target.y == 110){
-		targetTopBot=false;
-		}
-	}
-	if(!targetTopBot){
-		target.y-=0.5;
-		
-		if(target.y == 0){
-		targetTopBot=true;
-		}
-	}
-	ArrowCrash();
-	target.update();
-	backgroundALL.update();
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0; 
-	
+
+function control(){
+	MyHero.speedX = 0;
+    MyHero.speedY = 0; 
 	//Sterowanie strzalkami/////////////////
-    if (mapDOWN[37]/*left*/){
-		myGamePiece.speedX = -heroSpeed;
-	}
-	if (mapDOWN[39]/*right*/){
-		myGamePiece.speedX = heroSpeed;
-	}
+    if (mapDOWN[37]/*left*/){MyHero.speedX = -heroSpeed;}
+	if (mapDOWN[39]/*right*/){MyHero.speedX = heroSpeed;}
     if (mapDOWN[38]/*up*/){
 		if(lastkey){}
 		else{
-			myGamePiece.gravity = -0.6;
+			MyHero.gravity = -0.6;
 			jump = true;
 		}
 		lastkey = true;
 	}
-    if (mapDOWN[40]/*down*/){getRangedWeapon.reLoadArrow();}
+    if (mapDOWN[40]/*down*/ && !staticArrow){
+		getRangedWeapon.reLoadArrow();
+	}
 	//////////////////////////////////////
     if (mapPRESS[49]/*1*/){
 		if(weaponONE){
@@ -382,23 +455,35 @@ function updateGameArea() {
 	}
 	if (!(weaponTWO && mapDOWN[53]/*5*/)){
 		getRangedWeapon.old();
-	}
+	}	
+}
+
+function updateGameArea() {
+    myGameArea.clear();
 	
-	getWeapon.hand();
+	backgroundBottom.update();
+	backgroundTop.update();
+	
+	control();
+	MyHero.jumpHero();
+	MyHero.update();
 	getRangedWeapon.hand();
-	myGameArea.score();
-    myGamePiece.newPos(); 
-    myGamePiece.update();
+	getWeapon.hand();
+	
+	objectMove(target, 70, 110, 1);
+	objectMove(target2, 70, 110, 0.5);
+	objectMove(target3, 70, 110, 1);
+	objectMove(target4, 70, 110, 0.5);
+	
+	ArrowCrashFixed(target);
+	ArrowCrashFixed(target3);
+	ArrowCrashFixed(target4);
+	ArrowCrashFixed(target2);
+	ArrowCrashFixed(target5);
+
+
+  
+	myGameArea.score();  
 }
-function ArrowCrash(){
-	if(getRangedWeapon.crashArrow(target)){
-		arrowCrashTest[crashLength] = false;
-		arrowCrashStatic[crashLength] = getRangedWeapon.yT12;
-		fixedY[crashLength] = target.y;
-		crashLength++;
-		getRangedWeapon.reLoadArrow();
-	}
-	for(var i = 0, j = arrowCrashTest.length;i < j; i++)
-	getRangedWeapon.drawArrow(arrowCrashStatic[i], fixedY[i]);
-}
-startGame();
+
+addObject();
